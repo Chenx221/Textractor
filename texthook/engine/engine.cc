e8ec11b45177868ebd98cd6238f386a35a8bdc62
Chenx221 <i@chenx221.cyou>
2026-09-23 10:34:38 +0800

    feat(Siglus6): Add new function search signatures for サヤガタリ-妖刀の約束- (体験版)

--- texthook/engine/engine.cc
+++ texthook/engine/engine.cc
@@ -4796,16 +4796,41 @@ bool InsertSiglus6Hook(){
     0xFF, 0x75, XX
   };
 
+  // https://vndb.org/v64038 サヤガタリ -妖刀の約束-
+  const BYTE bytes2[] = {
+    0x55,
+    0x8B, 0xEC,
+    0x6A, XX,
+    0x68, XX4,
+    0x64, 0xA1, XX4,
+    0x50,
+    0x83, 0xEC, XX,
+    0x53,
+    0x56,
+    0x57,
+    0xA1, XX4,
+    0x33, 0xC5,
+    0x50,
+    0x8D, 0x45, XX,
+    0x64, 0xA3, XX4,
+    0x89, 0x55, XX,
+    0x8B, 0xF1, 
+    0x83, 0x7E
+  };
+
   ULONG range = max(processStopAddress - processStartAddress, MAX_REL_ADDR);
   ULONG addr = MemDbg::findBytes(bytes, sizeof(bytes), processStartAddress, processStartAddress + range);
   if (!addr) {
-    ConsoleOutput("vnreng:Siglus6: pattern not found");
-    return false;
+    addr = MemDbg::findBytes(bytes2, sizeof(bytes2), processStartAddress, processStartAddress + range);
+    if (!addr) {
+      ConsoleOutput("vnreng:Siglus6: pattern not found");
+      return false;
+    }
   }
   HookParam hp = {};
   hp.address = addr;
   hp.offset = pusha_ecx_off - 4;
-  hp.type = USING_UNICODE | USING_STRING;
+  hp.type = USING_UNICODE | USING_STRING | NO_CONTEXT;
   hp.text_fun = [](DWORD esp_base, HookParam*, BYTE, DWORD *data, DWORD *split, DWORD* len)
   {
     DWORD ptr = *(DWORD*)(esp_base + pusha_ecx_off - 4);
